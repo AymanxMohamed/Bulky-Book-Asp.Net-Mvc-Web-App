7b5c38dc84aa2330d5dd487428e8b47cd71d3670
Ayman Kheireldeen <aymanmido067@gmail.com>
2022-05-15 15:55:03 +0200

    Modifying Category Model Name To be required

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -3,6 +3,5 @@ public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-    public DbSet<Category> Categories { get; set; }
-
+    public DbSet<Category>? Categories { get; set; }
 }
--- Models/Category.cs
+++ Models/Category.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Bulky.Models;
 public class Category
 {
     public int Id { get; set; }
+    [Required]
     public string? Name { get; set; }
     public int DisplayOrder { get; set; }
     public DateTime CreatedDateTime { get; set; }  = DateTime.Now;
