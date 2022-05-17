7f8faa8fb01bc50d1ca6ff93255ef903f09cb73f
Ayman Kheireldeen <aymanmido067@gmail.com>
2022-05-17 15:45:11 +0200

    Adding MaxLengthOn Cover Type Name

--- Bulky.Models/CoverType.cs
+++ Bulky.Models/CoverType.cs
@@ -4,6 +4,9 @@ namespace Bulky.Models;
 public class CoverType
 {
     public int Id { get; set; }
+
     [Required]
+    [Display(Name = "Cover Type")]
+    [MaxLength(50)]
     public string Name { get; set; }
 }
